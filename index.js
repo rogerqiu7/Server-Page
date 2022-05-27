79ccf7c5a9be82e84804c5688b9da1aeccdb98a2
Roger Qiu <84350865+rogerqiu7@users.noreply.github.com>
2022-05-27 15:37:21 -0500

    initial upload

---
+++ index.js
@@ -0,0 +1,56 @@
+const http = require('http');
+const fs = require('fs');
+
+const htmlfiles = fs.readdirSync("./pages", { withFileTypes: true })
+                      .map(file => file.name);
+
+const cssfiles = fs.readdirSync("./styles/", { withFileTypes: true })
+                      .map(file => file.name);
+
+let homepage = '/';
+let page404;
+fs.readFile('./pages/404.html', (err, data) => {
+  page404 = data;
+});
+
+const server = http.createServer((req, res) => {
+  console.log('requested url: --V')
+  let filename = req.url;
+  let requested_dir;
+  filename = filename === homepage ? 'index' : filename.substr(1);
+  
+  if (htmlfiles.includes(filename + '.html')) {
+    console.log(`\n`, '--loading page--', `\n`)
+    filename += '.html'
+    requested_dir = "./pages/"
+  } else if (cssfiles.includes(filename)) {
+    console.log(`\n`, '--loading style--', `\n`)
+    requested_dir = "./styles/"
+  }
+  
+  console.log(`requesting==> ${requested_dir}${filename}`, ' ---')
+  fs.readFile(requested_dir + filename, (err, data) => {
+    if (err) {
+      console.log(`--${filename} not found--`)
+      res.writeHead(404, {'Content-Type': 'text/html'});
+      res.write(page404);
+      res.end();
+    } else {
+      if (requested_dir == "./pages/") {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        res.end();
+      } else if (requested_dir == "./styles/") {
+        res.writeHead(200, {'Content-Type': 'text/css'})
+        res.write(data);
+        res.end();
+      }
+    }
+  })
+
+})
+
+server.listen(3000, () => {
+  console.log('Server started! --- listening on port 3000')
+});
+
